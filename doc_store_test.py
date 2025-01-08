9a72038f09b311050afc3ff8a583c0e78ff5f0e4
Stefan-Bojilov <bojilov.stefan@gmail.com>
2025-01-08 12:31:39 +0100

    Pipeline with embedding loading

---
+++ doc_store_test.py
@@ -0,0 +1,92 @@
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack import Document
+from datasets import load_dataset
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.document_stores.types import DuplicatePolicy
+import pickle
+import os 
+import json
+import json
+from haystack import Pipeline
+from haystack.components.generators.chat import OpenAIChatGenerator
+from getpass import getpass
+import os
+import pickle
+from haystack.dataclasses import ChatMessage
+from haystack.components.builders import ChatPromptBuilder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack import Document
+from datasets import load_dataset
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.document_stores.types import DuplicatePolicy
+
+
+def load_document_store(file_path: str):
+    """
+    Load documents from a JSON file and initialize an InMemoryDocumentStore.
+
+    :param file_path: The path to the JSON file containing the document store data.
+    :return: An initialized InMemoryDocumentStore with the loaded documents.
+    """
+    document_store = InMemoryDocumentStore()
+    
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    
+    documents = data.get("documents", [])
+    print(f"Number of documents: {len(documents)}")
+    
+    # Load data into the DocumentStore
+    document_store.load_from_disk(path=file_path)
+    
+    return document_store
+
+document_store = InMemoryDocumentStore()
+load_document_store(file_path = '/Users/stefanbozhilov/Documents/GitHub/Capstone-RAG-project/data/DocStore.json')
+
+embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
+text_embedder = SentenceTransformersTextEmbedder(
+    model=embedding_model)
+
+retriever = InMemoryEmbeddingRetriever(document_store)
+
+template = [ChatMessage.from_user("""
+Given the following information, answer the question.
+
+Context:
+{% for document in documents %}
+    {{ document.content }}
+{% endfor %}
+
+Question: {{question}}
+Answer:
+""")]
+
+prompt_builder = ChatPromptBuilder(template=template)
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = "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"
+chat_generator = OpenAIChatGenerator(model="gpt-4o-mini")
+
+basic_rag_pipeline = Pipeline()
+# Add components to your pipeline
+basic_rag_pipeline.add_component("text_embedder", text_embedder)
+basic_rag_pipeline.add_component("retriever", retriever)
+basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+basic_rag_pipeline.add_component("llm", chat_generator)
+
+# Now, connect the components to each other
+basic_rag_pipeline.connect("text_embedder.embedding",
+                           "retriever.query_embedding")
+basic_rag_pipeline.connect("retriever", "prompt_builder")
+basic_rag_pipeline.connect("prompt_builder.prompt", "llm.messages")
+
+print(basic_rag_pipeline)
+question = "Which car model from Aston Martin is categorized as a Subcompact Car?"
+
+response = basic_rag_pipeline.run(
+    {"text_embedder": {"text": question}, "prompt_builder": {"question": question}})
+
+print(response["llm"]["replies"][0].text)
