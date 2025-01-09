e57f266b19e1af8318a545d2ac7ac0a98604e436
keesdh <kmdehoogh@gmail.com>
2025-01-09 11:16:31 +0100

    also answers

--- querydata.py
+++ querydata.py
@@ -5,6 +5,7 @@ answer_list = df_all['answer'].loc[0:228].tolist()
 chunk_id_list = df_all['chunk_id'].loc[0:228].tolist()
 text_list = df_all['text_description'].loc[0:228].tolist()
 
+# querys and texts
 query_dict = {}
 
 for query, text in zip(query_list, text_list):
@@ -13,6 +14,16 @@ for query, text in zip(query_list, text_list):
     query_dict[query].append(text)
 
 # [[query1, [id1, id2]], [query2, [id3, id4, id5]], ...]
-result = [[query, text] for query, text in query_dict.items()]
+query_text = [[query, text] for query, text in query_dict.items()]
 
-print(result[0])
+print(query_text[0])
+
+# querys and answers
+queryanswer = []
+
+for i in range(len(query_list)):
+    if i == 0 or [query_list[i], answer_list[i]] != queryanswer[-1]:
+        # [[query1, answer1], [query2, answer2], ...]
+        queryanswer.append([query_list[i], answer_list[i]])
+
+print(queryanswer[0])
