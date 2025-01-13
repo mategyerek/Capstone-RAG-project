e24790da37651fe49a0110cb976d72102e02a99b
mategyerek <materadio@gmail.com>
2025-01-13 13:32:43 +0100

    fix document merging

--- querydata.py
+++ querydata.py
@@ -1,9 +1,43 @@
+import os
+import json
 import pandas as pd
-df_all = pd.read_parquet('./data/test-00000-of-00002.parquet', engine='pyarrow')
+df_all = pd.read_parquet(
+    './data/test-00000-of-00002.parquet', engine='pyarrow')
+
+# merge documents
+df_all["document"] = df_all.groupby(
+    "query")["text_description"].transform(lambda x: '\n'.join(x))
+columns_of_interest = ["query", "answer", "document"]
+
+df_unique = df_all.drop_duplicates(
+    subset=columns_of_interest).dropna(subset=columns_of_interest)
+
+querys = df_unique['query'].tolist()
+answers = df_unique['answer'].tolist()
+texts = df_unique['document'].tolist()
+
+# Ensure the data folder exists
+os.makedirs('./data', exist_ok=True)
+
+# Save each list in separate files
+with open('./data/querys.json', 'w', encoding='utf-8') as f:
+    json.dump(querys, f, ensure_ascii=False, indent=4)
+
+with open('./data/texts.json', 'w', encoding='utf-8') as f:
+    json.dump(texts, f, ensure_ascii=False, indent=4)
+
+with open('./data/answers.json', 'w', encoding='utf-8') as f:
+    json.dump(answers, f, ensure_ascii=False, indent=4)
+
+print("Data saved in separate files: querys.json, texts.json, answers.json")
+
+
+"""
 query_list = df_all['query'].loc[0:228].tolist()
 answer_list = df_all['answer'].loc[0:228].tolist()
 chunk_id_list = df_all['chunk_id'].loc[0:228].tolist()
 text_list = df_all['text_description'].loc[0:228].tolist()
+
 
 # list of texts
 texts = []
@@ -36,22 +70,4 @@ for query in query_list[1:]:
 i = 1
 print(querys[i], texts[i], answers[i])
 
-import json
-import os
-
-# Ensure the data folder exists
-os.makedirs('./data', exist_ok=True)
-
-# Save each list in separate files
-with open('./data/querys.json', 'w', encoding='utf-8') as f:
-    json.dump(querys, f, ensure_ascii=False, indent=4)
-
-with open('./data/texts.json', 'w', encoding='utf-8') as f:
-    json.dump(texts, f, ensure_ascii=False, indent=4)
-
-with open('./data/answers.json', 'w', encoding='utf-8') as f:
-    json.dump(answers, f, ensure_ascii=False, indent=4)
-
-print("Data saved in separate files: querys.json, texts.json, answers.json")
-
-
+"""
